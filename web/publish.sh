d923e2f778de5354b4aface8cda88f278ef0f556
钱程 <mazey@mazey.net>
2021-03-11 00:14:16 +0800

    chore: 添加打包命令

---
+++ web/publish.sh
@@ -0,0 +1,14 @@
+# 由于服务器构建出错转为本地构建
+# 清空缓存
+rm -rf ./dist;
+git add .;
+git commit -m 'chore: 清空缓存';
+git push;
+
+# 更新版本
+npm run build;
+git add .;
+git commit -m 'chore: 更新版本';
+git push;
+
+echo '发布完成';
